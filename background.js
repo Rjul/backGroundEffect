fb57d1b95d3c041db6827b75f16e1020e713ecca
HUMMEL Julien <Juhummel@laposte.net>
2020-09-15 14:49:34 +0200

    Fixe deplacement
    Sliding 'ok'

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ var vhDeltaUp = 0.1;
 var vhDeltaDown = -0.1;
 var timeDelta = 10;
 var repeatPerScroll = 40;
-var positionY = 0;
+var positionY = 70;
 var heightBack = 15;
 var page = document.getElementById("page")
 
@@ -42,12 +42,16 @@ function backgroundMoveParametrage(down){
 }
 function moveBackGround(time, delta){
     setTimeout(() => {
-        positionY += delta;            
-        if(positionY < 0){positionY=0;}
+        positionY += delta;
+        heightBack -= delta;            
+        if(heightBack < 15){heightBack=15;}
+        if(positionY < 15){positionY=15;}
+        if(heightBack > 70){heightBack=70;}
+        if(positionY > 70){positionY=70;}
         console.log('VH posision '+positionY);
         console.log('increment Time '+time);
-        divPart1.style.transform = "translateY(-"+ positionY +"vh)";
-        divPart1.style.height = heightBack+"vh";
+        divPart1.style.height = positionY +"vh";
+        divPart2.style.height = heightBack+"vh";
         },time);
 }
 
